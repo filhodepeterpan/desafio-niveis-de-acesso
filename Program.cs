ac47aad3d19a8c0eaef779a28edc07e421375c25
Douglas Soares <douglassoares.cinema@gmail.com>
2024-09-17 03:06:27 -0300

    Fist Commit

---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+class Program{
+    static void Main(){
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        string[] funcionarios = {"Maria", "João", "Geraldo", "Márcia", "Camila", "Mauro", "Pedro", "Jhonatan", "Daiane", "Gustavo"};
+        string [] funcoes = {"Admnistrador", "Operador", "Operador", "Gerente", "Gerente", "Operador", "Operador", "Admnistrador", "Admnistrador", "Operador"};
+
+        int nivelDeAcessoMaria = 36;
+        int nivelDeAcessoJoao = 13;
+        int nivelDeAcessoGeraldo = 12;
+        int nivelDeAcessoMarcia = 19;
+        int nivelDeAcessoCamila = 28;
+        int nivelDeAcessoMauro = 15;
+        int nivelDeAcessoPedro = 20;
+        int nivelDeAcessoJhonatan = 58;
+        int nivelDeAcessoDaiane = 41;
+        int nivelDeAcessoGustavo = 18;
+        int nivelDeAcesso = 0;
+
+        string funcionarioAtual = "";
+        string funcaoAtual = "";
+        string permissao = "";
+
+        Console.WriteLine("FUNCIONÁRIO\tCARGO\t\t\tNÍVEL\t\tPERMISSÃO");
+        Console.Write("\n");
+
+        int indice = 0; // Variável para manter o índice e sincronizar funcionário com função
+
+        foreach(string funcionario in funcionarios){
+
+            if(funcionario == "Maria"){
+                funcionarioAtual = "Maria";
+                nivelDeAcesso = nivelDeAcessoMaria;
+            }
+            else if(funcionario == "João"){
+                funcionarioAtual = "João";
+                nivelDeAcesso = nivelDeAcessoJoao;
+            }
+            else if(funcionario == "Geraldo"){
+                funcionarioAtual = "Geraldo";
+                nivelDeAcesso = nivelDeAcessoGeraldo;
+            }
+            else if(funcionario == "Márcia"){
+                funcionarioAtual = "Márcia";
+                nivelDeAcesso = nivelDeAcessoMarcia;
+            }
+            else if(funcionario == "Camila"){
+                funcionarioAtual = "Camila";
+                nivelDeAcesso = nivelDeAcessoCamila;
+            }
+            else if(funcionario == "Mauro"){
+                funcionarioAtual = "Mauro";
+                nivelDeAcesso = nivelDeAcessoMauro;
+            }
+            else if(funcionario == "Pedro"){
+                funcionarioAtual = "Pedro";
+                nivelDeAcesso = nivelDeAcessoPedro;
+            }
+            else if(funcionario == "Jhonatan"){
+                funcionarioAtual = "Jhonatan";
+                nivelDeAcesso = nivelDeAcessoJhonatan;
+            }
+            else if(funcionario == "Daiane"){
+                funcionarioAtual = "Daiane";
+                nivelDeAcesso = nivelDeAcessoDaiane;
+            }
+            else if(funcionario == "Gustavo"){
+                funcionarioAtual = "Gustavo";
+                nivelDeAcesso = nivelDeAcessoGustavo;
+            }
+
+            funcaoAtual = funcoes[indice];
+
+            if(funcaoAtual.Contains("Admnistrador")){
+                funcaoAtual = "Administrador";
+                permissao = nivelDeAcesso > 55 ? "✅" : "⛔";
+            }
+            else if(funcaoAtual.Contains("Gerente")){
+                funcaoAtual = "Gerente";
+                permissao = nivelDeAcesso > 20 ? "✅" : "⛔";
+            }
+            else{
+                funcaoAtual = "Operador";
+                permissao = "⛔";
+            }
+
+            Console.WriteLine($"{funcionarioAtual.PadRight(12)}\t{funcaoAtual.PadRight(16)}\t{nivelDeAcesso.ToString().PadRight(12)}\t{permissao}");
+            Console.Write("\n");
+
+            indice++;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# Desafio Níveis de Acesso
+## Adaptação do desafio do Microsoft Learn
+&nbsp;
+
+O programa usa duas arrays: uma com o nome dos funcionários e o outro com as suas funções. É usado um foreach para percorrer a lista de funcionários, resgatar suas funções na outra array e conceder níveis de acesso baseado num nível. O código conta com alterações bastante consideráveis da proposta original (que buscava apenas avaliar o uso de expressões booleanas e operador ternário).
+&nbsp;
+
+&nbsp;
+
+**REGRAS DE NEGÓCIO**:
+- Administradores acima do nível 55: Permissão concedida ✅
+- Gerentes acima do nível 20: Permissão concedida ✅
+- Administradores de nível 55 ou menor: Permissão negada ⛔
+- Gerentes de nível 20 ou menor: Permissão negada ⛔
+- Operadores: Permissão negada ⛔
